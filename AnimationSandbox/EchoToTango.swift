cc442dbad2653d482e32da7fe5c6e0aafd3e3bb5
Fabian Knecht <fabianknecht@gmail.com>
2024-11-23 09:47:29 +0100

    Echo dot expanding to Tango line

--- AnimationSandbox/EchoToTango.swift
+++ AnimationSandbox/EchoToTango.swift
@@ -11,6 +11,11 @@ import SwiftUI
 struct EchoToTango: View {
     var body: some View {
         VStack{
+            HStack(spacing: 0) {
+                EchoLeft()
+                EchoBar()
+                EchoRight()
+            }
             HStack{
                 EchoOverlap()
                 Rectangle()
@@ -22,6 +27,66 @@ struct EchoToTango: View {
             }
            .padding(15)
         }
+}
+
+struct EchoBar: View {
+    let color = ETColourScheme.lime
+    @State var w = 0.0
+    var body: some View {
+        VStack(spacing: 0) {
+            Rectangle()
+                .frame(width: w, height: 25)
+                .foregroundColor(color)
+            Rectangle()
+                .frame(width: w, height: 70)
+                .foregroundColor(.white)
+                .onAppear() {
+                    withAnimation(.easeInOut.repeatForever(autoreverses: true).speed(0.5)) {
+                        w = 120
+                    }
+                }
+            Rectangle()
+                .frame(width: w, height: 25)
+                .foregroundColor(color)
+        }
+        .padding(0)
+        }
+}
+
+struct EchoLeft: View {
+    var body: some View {
+        ZStack{
+                Circle()
+                    .trim(from: 0.25, to: 0.75)
+                    .foregroundColor(ETColourScheme.lime)
+                    .frame(width: 120, height: 120)
+                    .offset(x: 65, y: 0)
+        Circle()
+            .trim(from: 0.25, to: 0.75)
+            .foregroundColor(.white)
+            .frame(width: 70, height: 70)
+            .offset(x: 65, y: 0)
+        }
+       }
+}
+
+struct EchoRight: View {
+    var body: some View {
+        ZStack{
+                Circle()
+                    .trim(from: 0.25, to: 0.75)
+                    .rotationEffect(.degrees(-180))
+                    .foregroundColor(ETColourScheme.lime)
+                    .frame(width: 120, height: 120)
+                    .offset(x: -65, y: 0)
+            Circle()
+                .trim(from: 0.25, to: 0.75)
+                .rotationEffect(.degrees(-180))
+                .foregroundColor(.white)
+                .frame(width: 70, height: 70)
+                .offset(x: -65, y: 0)
+        }
+    }
 }
 
 struct EchoCircle: View {
@@ -64,6 +129,7 @@ struct Tango: View {
 struct EchoToTango_Previews: PreviewProvider {
     static var previews: some View {
         HStack {
+            //EchoBar()
             EchoToTango()
         }
 
